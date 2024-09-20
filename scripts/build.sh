bf96fc697d289958371deccc8dc227fae70ef2b0
laulopezreal <laura.lopezr94@gmail.com>
2024-09-20 12:04:06 +0100

    Update scripts and config

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,5 @@ cd /usr/app/
 cp -r /src/* .
 cp config/biocypher_docker_config.yaml config/biocypher_config.yaml
 poetry install
-python3 create_knowledge_graph.py
+python3 scripts/target_disease_script.py
 chmod -R 777 biocypher-log
